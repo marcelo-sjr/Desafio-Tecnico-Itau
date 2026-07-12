4861d63f97ab92a3c3c0ff32da66e23d61697d93
marcelo-sjr <marcelodasilvajr2@gmail.com>
2026-07-12 07:37:46 -0300

    First commit

---
+++ src/main/java/com/itau/TransactionsApi/TransactionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.itau.TransactionsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsApiApplication.class, args);
+	}
+
+}
